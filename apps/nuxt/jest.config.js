6afddba10ec00f91fcd8060789c066cbc8014549
jigsaw <m@jgs.me>
2020-08-06 15:52:35 +0900

    Setup vue

---
+++ apps/nuxt/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'nuxt',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/nuxt',
+  snapshotSerializers: ['jest-serializer-vue'],
+};
